8cb78e62d5116e4add26bfcf21c4bc43d316358c
Antonio Terceiro <terceiro@debian.org>
2016-07-30 10:01:35 -0300

    test.pl: print a summary of failed test files

--- test.pl
+++ test.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 my $rc = 0;
+my @failed = ();
 
 if (scalar(@ARGV) == 0) {
   @ARGV = glob('test/test_*.sh');
@@ -13,8 +14,19 @@ for my $test (@ARGV) {
   printf("\n");
 
   if (system('sh', $test) != 0) {
+    push @failed, $test;
     $rc++;
   }
+}
+
+if (scalar(@failed) > 0 ) {
+  print "\n";
+  print "FAILED TEST FILES:\n";
+  print "------------------\n";
+  for my $failed (@failed) {
+    print "$failed\n";
+  }
+  print "\n";
 }
 
 exit($rc);
